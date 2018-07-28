bf750b491bfb9eb0d493c37b894e1d723dbba274
Megan Leet <megan.leet@iinet.net.au>
2018-07-28 21:45:02 +1000

    Add validation script.

---
+++ validate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import json
+
+def validate(path):
+	with open(path, 'rt') as f:
+		db = json.load(f)
+	for game_name, game in db.items():
+		if 'parent' in game:
+			parent_name = game['parent']
+			if parent_name in game_list:
+				parent = game_list[parent_name]
+				for key in parent.keys():
+					if key == "notes":
+						if key in game:
+							game_list[game_name][key] = parent[key] + ";" + game_list[game_name][key]
+						else:
+							game_list[game_name][key] = parent[key]
+					elif key not in game:
+						game_list[game_name][key] = parent[key]
+			else:
+				print('Oh no! {0} refers to undefined parent game {1}'.format(game_name, parent_name))
+			
+	
+validate('mac_db.json')
+validate('dos_db.json')

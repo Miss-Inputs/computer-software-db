df83d19256b07fc4a27f4703d1e3bc17b7e6b341
Megan Leet <megan.leet@iinet.net.au>
2018-07-28 21:55:02 +1000

    Update validation script to really validate stuff.

--- validate.py
+++ validate.py
@@ -2,7 +2,13 @@
 
 import json
 
-def validate(path):
+valid_keys = ['parent', 'creator_code', 'app_name', 'other_app_names', 'arch', 'category', 'clone_of', 'notes', 'resolution_compat', 'colours_compat', 'width', 'notes', 'width', 'height', 'colours', 'min_players', 'max_players', 'controls', 'runs_in_window', 'genre', 'subgenre', 'developer', 'publisher', 'year', 'compat_notes', 'requires_cd', 'adult', 'emu_compat', 'required_hardware', 'required_software']
+valid_arch = ['68k', 'ppc', 'fat']
+valid_resolution_compat = ['empty_space', 'nag', 'required']
+valid_colour_compat = ['palette', 'nag', 'required']
+
+
+def read_game_list(path):
 	with open(path, 'rt') as f:
 		game_list = json.load(f)
 	for game_name, game in game_list.items():
@@ -20,7 +26,17 @@ def validate(path):
 						game_list[game_name][key] = parent[key]
 			else:
 				print('Oh no! {0} refers to undefined parent game {1}'.format(game_name, parent_name))
-			
+				
+	return game_list
+
+def validate(path):
+	game_list = read_game_list(path)
+	for game_name, game in game_list.items():
+		for k, v in game.items():
+			if k not in valid_keys:
+				print(game_name, 'has invalid key:', k)
+				
+		#TODO: Check individual field values are valid
 	
 validate('mac_db.json')
 validate('dos_db.json')

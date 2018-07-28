05c4088b4134b209a7f5e0443ab1164b4229fbcc
Megan Leet <megan.leet@iinet.net.au>
2018-07-28 22:06:18 +1000

    Perform basic type checking in validation script.

--- validate.py
+++ validate.py
@@ -36,7 +36,44 @@ def validate(path):
 			if k not in valid_keys:
 				print(game_name, 'has invalid key:', k)
 				
-		#TODO: Check individual field values are valid
+		#TODO: Check "controls" object which will probably be tricky
+		
+		if 'arch' in game and game['arch'] not in valid_arch:
+			print(game_name, 'has invalid arch:', game['arch'])
+		if 'resolution_compat' in game and game['resolution_compat'] not in valid_resolution_compat:
+			print(game_name, 'has invalid resolution_compat:', game['resolution_compat'])
+		if 'colour_compat' in game and game['colour_compat'] not in valid_colour_compat:
+			print(game_name, 'has invalid colour_compat:', game['colour_compat'])
+			
+		if 'other_app_names' in game and not isinstance(game['other_app_names'], list):
+			print(game_name, 'has invalid other_app_names:', game['other_app_names'])
+		if 'width' in game and not isinstance(game['width'], int):
+			print(game_name, 'has invalid width:', game['width'])
+		if 'height' in game and not isinstance(game['height'], int):
+			print(game_name, 'has invalid height:', game['height'])
+		if 'colours' in game and not isinstance(game['colours'], int):
+			print(game_name, 'has invalid colours:', game['colours'])
+		if 'min_players' in game and not isinstance(game['min_players'], int):
+			print(game_name, 'has invalid min_players:', game['min_players'])
+		if 'max_players' in game and not isinstance(game['max_players'], int):
+			print(game_name, 'has invalid max_players:', game['max_players'])
+		if 'controls' in game and not isinstance(game['controls'], dict):
+			print(game_name, 'has invalid controls:', type(game['controls']))
+		if 'year' in game and not isinstance(game['year'], int):
+			print(game_name, 'has invalid year:', game['year'])
+		if 'runs_in_window' in game and not isinstance(game['runs_in_window'], bool):
+			print(game_name, 'has invalid runs_in_window:', game['runs_in_window'])
+		if 'requires_cd' in game and not isinstance(game['requires_cd'], bool):
+			print(game_name, 'has invalid requires_cd:', game['requires_cd'])
+		if 'adult' in game and not isinstance(game['adult'], bool):
+			print(game_name, 'has invalid adult:', game['adult'])
+		if 'emu_compat' in game and not isinstance(game['emu_compat'], dict):
+			print(game_name, 'has invalid emu_compat:', type(game['emu_compat']))
+		if 'required_hardware' in game and not isinstance(game['required_hardware'], dict):
+			print(game_name, 'has invalid required_hardware:', type(game['required_hardware']))
+		if 'required_software' in game and not isinstance(game['required_software'], dict):
+			print(game_name, 'has invalid required_software:', type(game['required_software']))
+
 	
 validate('mac_db.json')
 validate('dos_db.json')
